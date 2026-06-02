e7f303b94da040c3c64455c0aaf3e1882a332d3f
malusccp <luizamorais2007@gmail.com>
2026-06-02 03:11:23 -0300

    feat: adicionar validação de caracteres em nomes de funcionários

---
+++ componentes-do-bd/triggers/trg_nomes_nao_numeros.sql
@@ -0,0 +1,15 @@
+create or replace function validar_nome_funcionario()
+returns trigger as $$
+begin
+    if new.nome_funcionario ~ '.*\d.*' then
+        raise exception 'não é permitido números dentro do nome do funcionário. valor fornecido: "%".', new.nome_funcionario;
+    end if;
+
+    return new;
+end;
+$$ language plpgsql;
+
+create trigger trg_nomes_nao_numeros
+before insert or update on funcionario
+for each row
+execute function valida_nome_funcionario();
