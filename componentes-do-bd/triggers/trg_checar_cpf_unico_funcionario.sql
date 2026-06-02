1d780572db9d244d594772ebc1978292a191e8a0
malusccp <luizamorais2007@gmail.com>
2026-06-02 03:09:56 -0300

    feat: adicionar validação de CPF único para funcionários

---
+++ componentes-do-bd/triggers/trg_checar_cpf_unico_funcionario.sql
@@ -0,0 +1,21 @@
+create or replace function checar_cpf_unico_funcionario()
+returns trigger as $$
+begin
+    if exists (
+        select 1
+        from funcionario
+        where cpf = new.cpf
+        and id_funcionario is distinct from new.id_funcionario  
+    ) then
+       
+        raise exception 'Erro: Já existe um funcionário cadastrado com o cpf %.', new.cpf;
+    end if;
+
+    return new;
+end;
+$$ language plpgsql;
+
+create or replace trigger trg_checar_cpf_unico_funcionario
+before insert or update on funcionario
+for each row
+execute function checar_cpf_unico_funcionario();
