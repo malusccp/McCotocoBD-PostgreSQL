3c9097ed6f2a5baaba0a6945967099b4d3144299
Isaacst07 <isaacjsantos07@gmail.com>
2026-06-10 04:46:36 -0300

    :recycle: refactor: Melhorando a validação da franquia do funcionário

--- componentes-do-bd/triggers/trg_validacao_de_venda_pela_franquia_funcionario.sql
+++ componentes-do-bd/triggers/trg_validacao_de_venda_pela_franquia_funcionario.sql
@@ -2,7 +2,7 @@ CREATE OR REPLACE FUNCTION validar_venda()
 RETURNS TRIGGER
 AS $$
 BEGIN
-	IF ((SELECT id_franquia FROM funcionario WHERE id_funcionario = NEW.id_funcionario) != NEW.id_franquia) THEN
+	IF ((SELECT id_franquia FROM funcionario WHERE id_funcionario = NEW.id_funcionario) IS DISTINCT FROM NEW.id_franquia) THEN
 		RAISE EXCEPTION 'Essa venda não pode ser concluída, pois o funcionário que a fez não é dessa franquia.';
 	END IF;
 	
