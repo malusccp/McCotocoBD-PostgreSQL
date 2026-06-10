9d4d739026609bed6e6d66a0c2fd3b2edc384e73
Isaacst07 <isaacjsantos07@gmail.com>
2026-06-10 04:21:46 -0300

    :sparkles: feat: Adicionando função que calcula valor total para ajudar na função venda

---
+++ componentes-do-bd/functions/fn_calcular_valor_total.sql
@@ -0,0 +1,28 @@
+CREATE OR REPLACE FUNCTION calcular_valor_total(p_itens jsonb)
+RETURNS FLOAT
+AS $$
+DECLARE 
+    v_valor_total float := 0; 
+    v_valor_calculado float;
+    item jsonb;
+BEGIN
+    FOR item IN SELECT * FROM jsonb_array_elements(p_itens)
+    LOOP 
+        SELECT (preco_base * (item ->> 'qtd')::int) 
+        INTO v_valor_calculado
+        FROM produto 
+        WHERE id_produto = (item ->> 'id_produto')::int;
+    
+        v_valor_total := v_valor_total + v_valor_calculado;
+        
+    END LOOP; 
+    
+    RETURN v_valor_total;
+    
+END;
+$$ LANGUAGE plpgsql;
+
+
+
+
+SELECT * FROM produto;
