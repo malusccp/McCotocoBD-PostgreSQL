47da3c6d7a4a638f04018d59a0074ca7ecf52e01
Isaacst07 <isaacjsantos07@gmail.com>
2026-06-10 03:43:09 -0300

    :sparles: feat: Adicinando Trigger que garante regra de negócio que um funcionário só pode realizar uma venda da franquia que ele atua

---
+++ componentes-do-bd/triggers/trg_validacao_de_venda_pela_franquia_funcionario.sql
@@ -0,0 +1,16 @@
+CREATE OR REPLACE FUNCTION validar_venda()
+RETURNS TRIGGER
+AS $$
+BEGIN
+	IF ((SELECT id_franquia FROM funcionario WHERE id_funcionario = NEW.id_funcionario) != NEW.id_franquia) THEN
+		RAISE EXCEPTION 'Essa venda não pode ser concluída, pois o funcionário que a fez não é dessa franquia.';
+	END IF;
+	
+	RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER validacao_de_venda_pela_franquia_funcionario
+BEFORE INSERT ON pedido
+FOR EACH ROW
+EXECUTE FUNCTION validar_venda();
