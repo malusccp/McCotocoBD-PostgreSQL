ca7f1e16747514e6b6f9fb796698dbdf7e060eb0
Isaacst07 <isaacjsantos07@gmail.com>
2026-06-14 23:16:41 -0300

    :sparkles: feat: Adicionando o role Gerente

---
+++ componentes-do-bd/roles/role_gerente.sql
@@ -0,0 +1,18 @@
+CREATE ROLE gerente;
+
+GRANT atendente_caixa TO gerente;
+
+GRANT SELECT, INSERT, UPDATE ON funcionario TO gerente;
+GRANT USAGE, SELECT ON SEQUENCE funcionario_id_funcionario_seq TO gerente;
+
+GRANT SELECT, INSERT, UPDATE ON estoque_unid TO gerente;
+
+GRANT SELECT, INSERT, UPDATE ON meta, meta_franquia TO gerente;
+GRANT USAGE, SELECT ON SEQUENCE meta_id_meta_seq, meta_franquia_id_meta_vigente_seq TO gerente;
+
+GRANT EXECUTE ON FUNCTION inserir(TEXT, JSONB) TO gerente;
+GRANT EXECUTE ON FUNCTION atualizar(TEXT, TEXT, TEXT, TEXT, TEXT, TEXT) TO gerente;
+GRANT EXECUTE ON FUNCTION deletar(TEXT, JSONB) TO gerente;
+
+GRANT SELECT ON franquia TO gerente;
+
