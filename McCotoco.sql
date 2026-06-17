e13aa30d6cc75a135777a339d951cd05bdd173d1
Isaacst07 <isaacjsantos07@gmail.com>
2026-06-17 14:54:39 -0300

    :recycle: refactor: AJustando função inserir crud

--- McCotoco.sql
+++ McCotoco.sql
@@ -606,26 +606,40 @@ FOR EACH ROW EXECUTE FUNCTION fn_auditoria_generica();
 -- 3.1. FUNÇÕES GENÉRICAS DE MANIPULAÇÃO DE DADOS
 ------------------------------------------------------------------------------------
 
-CREATE OR REPLACE FUNCTION inserir(nome_tabela TEXT, valores JSONB)
-RETURNS VOID AS $$
-DECLARE
-    mensagem_erro   text;
+CREATE OR REPLACE FUNCTION inserir (nome_tabela TEXT, valores JSONB)
+RETURNS VOID
+AS $$
+DECLARE 
+    mensagem_erro text;
     codigo_sqlstate text;
-BEGIN
-    EXECUTE format('INSERT INTO %I SELECT * FROM jsonb_populate_record(NULL::%I, $1)', nome_tabela, nome_tabela)
+    v_colunas text; 
+BEGIN 
+    SELECT string_agg(quote_ident(chaves), ', ') 
+    INTO v_colunas 
+    FROM jsonb_object_keys(valores) as chaves;
+
+    IF v_colunas IS NULL THEN
+        RAISE EXCEPTION 'O valores fornecido estão vazios.';
+    END IF;
+
+    EXECUTE format('INSERT INTO %I (%s) SELECT %s FROM jsonb_populate_record(NULL::%I, $1)', 
+                   nome_tabela, v_colunas, v_colunas, nome_tabela)
     USING valores;
+
     RAISE NOTICE 'Dados inseridos com sucesso na tabela %!', nome_tabela;
-EXCEPTION
+
+EXCEPTION 
     WHEN OTHERS THEN
         GET STACKED DIAGNOSTICS
-            mensagem_erro   = MESSAGE_TEXT,
+            mensagem_erro = MESSAGE_TEXT,
             codigo_sqlstate = RETURNED_SQLSTATE;
-        IF codigo_sqlstate = '42501' THEN
+
+        IF codigo_sqlstate = '42501' THEN 
             RAISE EXCEPTION 'Acesso negado. O seu perfil não tem permissão para inserir na tabela "%".', nome_tabela;
-        ELSIF codigo_sqlstate = 'P0001' THEN
+        ELSIF codigo_sqlstate = 'P0001' THEN 
             RAISE EXCEPTION 'Erro ao inserir dados na tabela "%". %. Por favor, verifique os dados fornecidos.', nome_tabela, mensagem_erro;
-        ELSE
-            RAISE EXCEPTION 'Erro ao inserir dados na tabela "%". Por favor, verifique os dados fornecidos ou a estrutura da tabela. (Erro: %)', nome_tabela, SQLERRM;
+        ELSE 
+            RAISE EXCEPTION 'Erro ao inserir dados na tabela "%". Por favor, verifique os dados fornecidos ou a estrutura da tabela. (Erro: %)', nome_tabela, mensagem_erro;
         END IF;
 END;
 $$ LANGUAGE plpgsql;
--- componentes-do-bd/functions/fn_inserir.sql
+++ componentes-do-bd/functions/fn_inserir.sql
@@ -1,42 +1,37 @@
-CREATE OR REPLACE
-FUNCTION inserir (nome_tabela TEXT,
-valores JSONB)
+CREATE OR REPLACE FUNCTION inserir (nome_tabela TEXT, valores JSONB)
 RETURNS VOID
 AS $$
 DECLARE 
-
     mensagem_erro text;
-
-	codigo_sqlstate text;
-
+    codigo_sqlstate text;
+    v_colunas text; 
 BEGIN 
+    SELECT string_agg(quote_ident(chaves), ', ') 
+    INTO v_colunas 
+    FROM jsonb_object_keys(valores) as chaves;
 
-    EXECUTE format('INSERT INTO %I SELECT * FROM jsonb_populate_record(NULL::%I, $1)', nome_tabela, nome_tabela)
+    IF v_colunas IS NULL THEN
+        RAISE EXCEPTION 'O valores fornecido estão vazios.';
+    END IF;
+
+    EXECUTE format('INSERT INTO %I (%s) SELECT %s FROM jsonb_populate_record(NULL::%I, $1)', 
+                   nome_tabela, v_colunas, v_colunas, nome_tabela)
     USING valores;
 
-RAISE NOTICE 'Dados inseridos com sucesso na tabela %!',
-nome_tabela;
+    RAISE NOTICE 'Dados inseridos com sucesso na tabela %!', nome_tabela;
 
-EXCEPTION
-WHEN OTHERS THEN
+EXCEPTION 
+    WHEN OTHERS THEN
         GET STACKED DIAGNOSTICS
-        
             mensagem_erro = MESSAGE_TEXT,
             codigo_sqlstate = RETURNED_SQLSTATE;
 
-IF codigo_sqlstate = '42501' THEN 
-            RAISE EXCEPTION 'Acesso negado. O seu perfil não tem  permissão para inserir na tabela "%".',
-nome_tabela;
-
-ELSIF codigo_sqlstate = 'P0001' THEN 
-            RAISE EXCEPTION 'Erro ao inserir dados na tabela "%". %. Por favor, verifique os dados fornecidos.',
-nome_tabela,
-mensagem_erro;
-ELSE 
-            RAISE EXCEPTION 'Erro ao inserir dados na tabela "%". Por favor, verifique os dados fornecidos ou a estrutura da tabela. (Erro: %)',
-nome_tabela,
-SQLERRM;
-END IF;
+        IF codigo_sqlstate = '42501' THEN 
+            RAISE EXCEPTION 'Acesso negado. O seu perfil não tem permissão para inserir na tabela "%".', nome_tabela;
+        ELSIF codigo_sqlstate = 'P0001' THEN 
+            RAISE EXCEPTION 'Erro ao inserir dados na tabela "%". %. Por favor, verifique os dados fornecidos.', nome_tabela, mensagem_erro;
+        ELSE 
+            RAISE EXCEPTION 'Erro ao inserir dados na tabela "%". Por favor, verifique os dados fornecidos ou a estrutura da tabela. (Erro: %)', nome_tabela, mensagem_erro;
+        END IF;
 END;
-
 $$ LANGUAGE plpgsql;
